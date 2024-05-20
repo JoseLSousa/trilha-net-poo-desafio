bc138f2133787f651f8a4feecd104c11cdf1713b
José Lucas <lucassousasertania@gmail.com>
2024-05-20 16:59:10 -0300

    Feat: Implementação da herança e propiedades

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -2,14 +2,20 @@ namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
-        public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
-
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
             // TODO: Passar os parâmetros do construtor para as propriedades
         }
+
+        public string Numero { get; set; }
+        private string Modelo;
+        private string IMEI;
+        private int Memoria;
+        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
 
         public void Ligar()
         {
--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia NokiaTijolao = new(numero: "40028922", modelo: "1100", imei: "imei1234", memoria: 2);
+Iphone Iphone5 = new(numero: "40028922", modelo: "Iphone5s", imei: "imei4532", memoria: 4);
+
+
+NokiaTijolao.Ligar();
+NokiaTijolao.ReceberLigacao();
+NokiaTijolao.InstalarAplicativo("WhatsApp");
+
+
+Iphone5.Ligar();
+Iphone5.InstalarAplicativo("Facebook");
+Iphone5.ReceberLigacao();
